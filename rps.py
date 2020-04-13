c6e9db48ac621a6c0fa32375352927f684cf6bd6
Akhil Kathuria <imakathuria@gmail.com>
2020-04-13 18:40:52 +0530

    performed step 2

--- rps.py
+++ rps.py
@@ -1,1 +1,42 @@
 dbskjdf.dnfndfdnfbknaf v,. nvand;vna fvkjsn;jvnjfnvsnv;jj;vlslnvjsn;jvjnsd/ln/ln/l
+
+
+Skip to content
+Using Gmail with screen readers
+
+1 of 4,476
+A brand new series from NPTEL is available from April 13, 2020 onward
+Inbox
+x
+
+online course <onlinecourses@nptel.iitm.ac.in> Unsubscribe
+4:51 PM (1 hour ago)
+to cs-announce
+
+Dear learner,
+
+Hope all are at home, safe and well.
+
+Want to learn about cutting edge technology?
+Career choices?
+Research areas?  
+
+A brand new series from NPTEL is available from April 13, 2020 onward.
+Don't miss our Live sessions by the experts in the fields! 
+
+Link: bit.ly/NPTELLIVE - Special Lecture Series
+
+We hope that you join and make use of this opportunity.
+And do tell your friends.
+
+
+Happy Learning!
+NPTEL TEAM
+--
+You received this message because you are subscribed to the Google Groups "Announcements group for CS courses" group.
+To unsubscribe from this group and stop receiving emails from it, send an email to cs-announce+unsubscribe@nptel.iitm.ac.in.
+To view this discussion on the web visit https://groups.google.com/a/nptel.iitm.ac.in/d/msgid/cs-announce/CAFi4wK--m4wTceA-tk11bZjD5h_8gObAaqH4mrQhJYfiL6dckQ%40mail.gmail.com.
+--
+You received this message because you are subscribed to the Google Groups "Announcement list for AI:Knowledge Representation and Reasoning" group.
+To unsubscribe from this group and stop receiving emails from it, send an email to noc19-cs19-announce+unsubscribe@nptel.iitm.ac.in.
+To view this discussion on the web visit https://groups.google.com/a/nptel.iitm.ac.in/d/msgid/noc19-cs19-announce/CAFi4wK--m4wTceA-tk11bZjD5h_8gObAaqH4mrQhJYfiL6dckQ%40mail.gmail.com.
